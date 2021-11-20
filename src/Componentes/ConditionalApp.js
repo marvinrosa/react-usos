bd2fd4360247703434e85f7b8d4c2925b42fd4fc
Marvin Rosa <marvin.fortin@outlook.com>
2021-11-20 09:00:48 -0600

    showing a message with a codition

--- src/Componentes/ConditionalApp.js
+++ src/Componentes/ConditionalApp.js
@@ -5,8 +5,9 @@ const ConditionalApp = () => {
   return (
     <>
       <h1>Contidional App</h1>
-      <button onClick={setContidion(!condition)}>Toggle</button>
-      {condition ? <h2>Im a message</h2> : null}
+      <button onClick={() => setContidion(!condition)}>Toggle</button>
+      {/* {condition ? <h2>Im a message</h2> : <h2>Other message</h2>} */}
+      {condition && <h2>Im a message</h2>}
     </>
   );
 };
